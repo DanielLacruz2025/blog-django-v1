8f4f20fdf486d107787c4cd96b3a08c50580c25c
Daniel <alejandrodelacruz1001@gmail.com>
2025-03-13 12:12:36 -0400

    publication create v1 without link

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from .models import Publication
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
-
-class PublicationListView (ListView):
+class PublicationListView(ListView):
     model = Publication
-    template_name = 'publications-list.html'
+    template_name = 'publications-list.html'  
 
 class PublicationDetailView(DetailView):
     model = Publication
-    template_name = 'publication-detail.html'
+    template_name = 'publication-detail.html'  
+
+class PublicationCreateView(CreateView):
+    model = Publication
+    template_name = 'publication-create.html'  
+    fields = ['title', 'body', 'author']  
+        
