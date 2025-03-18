a2d0fdad9f496d07b45312627814359f25a1e9dc
Daniel <alejandrodelacruz1001@gmail.com>
2025-03-18 17:41:12 -0400

    publication delete done

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,13 @@ from .views import (
                         PublicationDetailView,
                         PublicationCreateView,
                         PublicationUpdateView,
-                    )
+                        PublicationDeleteView
+                        )
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publications-list'),
     path('publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
     path('publication/new', PublicationCreateView.as_view(), name='publication-create'),
     path('publication/<int:pk>/update', PublicationUpdateView.as_view(), name='publication-update'),
+    path('publication/<int:pk>/delete', PublicationDeleteView.as_view(), name='publication-delete'),
 ]
