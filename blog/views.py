e9278b2f8b47d1e3216552ddc477ee93c1a55d47
Daniel <alejandrodelacruz1001@gmail.com>
2025-03-25 18:02:15 -0400

    permissions done

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from .models import Publication
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin # type: ignore
+from django.contrib.auth.mixins import (
+                                        LoginRequiredMixin,
+                                        UserPassesTestMixin
+                                        ) # type: ignore
 
 # Create your views here.
 class PublicationListView(ListView):
@@ -18,14 +21,20 @@ class PublicationCreateView(LoginRequiredMixin,CreateView):
     template_name = 'publication-create.html'  
     fields = ['title', 'body', 'author']  
 
-class PublicationUpdateView(LoginRequiredMixin,UpdateView):
+class PublicationUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Publication
     template_name = 'publication-update.html'  
     fields = ['title', 'body']
 
-class PublicationDeleteView(LoginRequiredMixin,DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class PublicationDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Publication
     template_name = 'publication-delete.html'
     success_url = reverse_lazy ('publications-list')
 
-    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
